472b2cb60756d156dce3554091b65d6779a5b3e6
ChamilkaMihiraj2002 <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2024-09-05 23:55:17 +0530

    Create ArrayListClass.java

---
+++ Day - 11/Java/ArrayListClass.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListClass {
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        stringArrayList.add("Monday");
+        stringArrayList.add("Tuesday");
+        stringArrayList.add("Wednesday");
+        stringArrayList.add("Thursday");
+        stringArrayList.add("Friday");
+        stringArrayList.add("Saturday");
+        stringArrayList.add("Sunday");
+
+        System.out.println(stringArrayList);
+    }
+}
