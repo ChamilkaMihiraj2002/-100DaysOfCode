30dab7763fda91e88b8082fa58251df1da3c6e7a
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2024-09-28 23:51:39 +0530

    Create DeleteLastNodeP.c

---
+++ Data Structures & Algorithms/Single Linked List/DeleteLastNodeP.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node * link;
+};
+
+void count_of_node(struct node *);
+void display(struct node *);
+void del_last(struct node *);
+
+int main() {
+    struct node *  head = (struct node *) malloc (sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *  current = (struct node *) malloc (sizeof(struct node));
+    current->data = 50;
+    current->link = NULL;
+
+    head->link = current;
+    
+    current = (struct node *) malloc (sizeof(struct node));
+    current->data = 75;
+    current->link = NULL;
+
+    head->link->link = current;
+
+    count_of_node(head);
+    display(head);
+
+    del_last(head);
+
+    count_of_node(head);
+    display(head);
+
+    return 0;
+}
+
+void count_of_node(struct node * head) {
+    int count = 0;
+    if (head == NULL){  printf("Linked list is Empty!!\n"); }
+
+    // create a pointer
+    struct node * ptr = NULL;
+    ptr = head;
+
+    while (ptr != NULL)
+    {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("Count of Node :: %d\n", count);
+}
+
+void display(struct node * head) {
+    if (head == NULL){  printf("Linked list is Empty!!\n"); }
+
+    // Craete a pointer
+    struct node * ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
+}
+
+void del_last(struct node * head) {
+    if (head == NULL){
+        printf("Linked List is Empty!!");
+    } else if (head->link == NULL) {
+        free(head);
+        head = NULL;
+    } else {
+        struct node * temp = head;
+        while (temp->link->link != NULL)
+        {
+            temp = temp->link;
+        }
+        free(temp->link);
+        temp->link = NULL;
+    }
+}
