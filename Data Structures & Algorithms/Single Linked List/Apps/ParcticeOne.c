48523c8843d53b34f6c9dcc705b78d4391be79f0
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2024-10-09 00:10:00 +0530

    Create ParcticeOne.c

---
+++ Data Structures & Algorithms/Single Linked List/Apps/ParcticeOne.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node * link;
+};
+
+struct node * head;
+
+void display();
+void insertBegin();
+void insertEnd();
+void countNode();
+
+int InputInt();
+
+int main() {
+    head = (struct node *) malloc (sizeof(struct node));
+    head->data = 5;
+    head->link = NULL;
+
+    for (int i = 0; i < 5; i++)
+    {
+        insertEnd();
+    }
+
+    display();
+
+    insertBegin();
+    
+    display();
+    
+    return 0;
+}
+
+void display() {
+    struct node * ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
+}
+
+void insertBegin() {
+    struct node * temp = (struct node * ) malloc (sizeof(struct node));
+    
+    temp->data = InputInt();
+    temp->link = NULL;
+
+    temp->link = head;
+    head = temp;    
+}
+
+void insertEnd() {
+    struct node * temp = (struct node * ) malloc (sizeof(struct node));
+    
+    temp->data = InputInt();
+    temp->link = NULL;
+
+    if (head == NULL)
+    {
+        head = temp;
+    } else {
+        struct node * ptr = head;
+        while (ptr->link != NULL) {
+            ptr = ptr->link;
+        }
+        ptr->link = temp;
+    }
+    
+
+}
+
+void countNode() {
+    
+}
+
+int InputInt() {
+    int value;
+    printf("Enter number :: ");
+    scanf("%d", &value);
+    return value;
+}
